b31f811ecdd3e6c6171ce820199875cd4ee1d4c8
choiseungho <jackpot97912@gmail.com>
2024-03-20 17:47:31 +0900

    2024-03-20 UPDATE Article

--- src/main/java/com/erichgamma/api/user/User.java
+++ src/main/java/com/erichgamma/api/user/User.java
@@ -1,13 +1,19 @@
 package com.erichgamma.api.user;
 
+import jakarta.persistence.*;
 import lombok.*;
 
+@Entity(name="users")
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Getter
 @ToString(exclude = "id")
 public class User {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
     private Long id;
     private String username;
     private String password;

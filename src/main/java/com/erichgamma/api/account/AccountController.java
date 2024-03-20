53c7c2b0b40220a9f99be3ce3c98ff4ee7235a1e
choiseungho <jackpot97912@gmail.com>
2024-03-20 17:55:34 +0900

    tt

--- src/main/java/com/erichgamma/api/account/AccountController.java
+++ src/main/java/com/erichgamma/api/account/AccountController.java
@@ -1,84 +1,84 @@
-//package com.erichgamma.api.account;
-//
-//
-//import com.erichgamma.api.common.UtilService;
-//import com.erichgamma.api.common.UtilServiceImpl;
-//import com.erichgamma.api.enums.Messenger;
-//import lombok.Getter;
-//import lombok.RequiredArgsConstructor;
-//
-//import java.util.Date;
-//import java.util.List;
-//import java.util.Map;
-//
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequiredArgsConstructor
-//public class AccountController {
-//    private final AccountServiceImpl accountService;
-//    private final UtilService utilService;
-//
-//    public Messenger creatAccount(@RequestBody Map<String, ?> reqMap) {
-//        System.out.println("=== Create Account ===");
-//        System.out.println("Input(Account Number, Depositor)");
-//        return accountService.save(Account.builder()
-//                .id((long) utilService.createRandomInt(1, 100))
-//                .accountNumber("")
-//                .accountHolder("")
-//                .balance(0.0)
-//                .transactionDate(new Date())
-//                .build());
-//    }
-//
-//    public String getAccountsList() {
-//        System.out.println("== Account List ===");
-//        List<?> list = accountService.findAll();
-//        list.forEach(System.out::println);
-//        return "------------------";
-//    }
-//
-//    public String withdraw(@RequestBody Map<String, ?> reqMap) {
-//        System.out.println("=== Withdraw ===");
-//        System.out.println("Input(Account Number, amount)");
-//        return accountService.withdraw(Account.builder()
-//                .accountNumber("")
-//                .balance(Double.parseDouble(""))
-//                .build());
-//    }
-//
-//    public String deposit(@RequestBody Map<String, ?> reqMap) {
-//        System.out.println("=== Deposit ===");
-//        System.out.println("Input(Account Number, amount)");
-//        return accountService.deposit(Account.builder()
-//                .accountNumber("")
-//                .balance(Double.parseDouble(""))
-//                .build());
-//    }
-//
-//    public String getBalance(@RequestBody Map<String, ?> reqMap) {
-//        System.out.println("=== Get Balance ===");
-//        System.out.println("Input(Account Number)");
-//        return accountService.getBalance(Account.builder()
-//                .accountNumber("")
-//                .build());
-//    }
-//
-//    public Messenger deleteAccount(@RequestBody Map<String, ?> reqMap) {
-//        System.out.println("=== Delete Account ===");
-//        System.out.println("Input(Account Number)");
-//        return accountService.delete(Account.builder()
-//                .accountNumber("")
-//                .build());
-//    }
-//
-//    public String findAccount(@RequestBody Map<String, ?> reqMap) {
-//        System.out.println("=== Find Account ===");
-//        System.out.println("Input(Account Number)");
-//        return accountService.getOne("")
-//                .orElse(new Account())
-//                .toString();
-//    }
-//}
+package com.erichgamma.api.account;
+
+
+import com.erichgamma.api.common.UtilService;
+import com.erichgamma.api.common.UtilServiceImpl;
+import com.erichgamma.api.enums.Messenger;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AccountController {
+    private final AccountServiceImpl accountService;
+    private final UtilService utilService;
+
+    public Messenger creatAccount(@RequestBody Map<String, ?> reqMap) {
+        System.out.println("=== Create Account ===");
+        System.out.println("Input(Account Number, Depositor)");
+        return accountService.save(Account.builder()
+                .id((long) utilService.createRandomInt(1, 100))
+                .accountNumber("")
+                .accountHolder("")
+                .balance(0.0)
+                .transactionDate(new Date())
+                .build());
+    }
+
+    public String getAccountsList() {
+        System.out.println("== Account List ===");
+        List<?> list = accountService.findAll();
+        list.forEach(System.out::println);
+        return "------------------";
+    }
+
+    public String withdraw(@RequestBody Map<String, ?> reqMap) {
+        System.out.println("=== Withdraw ===");
+        System.out.println("Input(Account Number, amount)");
+        return accountService.withdraw(Account.builder()
+                .accountNumber("")
+                .balance(Double.parseDouble(""))
+                .build());
+    }
+
+    public String deposit(@RequestBody Map<String, ?> reqMap) {
+        System.out.println("=== Deposit ===");
+        System.out.println("Input(Account Number, amount)");
+        return accountService.deposit(Account.builder()
+                .accountNumber("")
+                .balance(Double.parseDouble(""))
+                .build());
+    }
+
+    public String getBalance(@RequestBody Map<String, ?> reqMap) {
+        System.out.println("=== Get Balance ===");
+        System.out.println("Input(Account Number)");
+        return accountService.getBalance(Account.builder()
+                .accountNumber("")
+                .build());
+    }
+
+    public Messenger deleteAccount(@RequestBody Map<String, ?> reqMap) {
+        System.out.println("=== Delete Account ===");
+        System.out.println("Input(Account Number)");
+        return accountService.delete(Account.builder()
+                .accountNumber("")
+                .build());
+    }
+
+    public String findAccount(@RequestBody Map<String, ?> reqMap) {
+        System.out.println("=== Find Account ===");
+        System.out.println("Input(Account Number)");
+        return accountService.getOne("")
+                .orElse(new Account())
+                .toString();
+    }
+}
--- src/main/java/com/erichgamma/api/account/AccountServiceImpl.java
+++ src/main/java/com/erichgamma/api/account/AccountServiceImpl.java
@@ -1,95 +1,97 @@
-//package com.erichgamma.api.account;
-//
-//import com.erichgamma.api.common.AbstractService;
-//import com.erichgamma.api.enums.Messenger;
-//import lombok.Getter;
-//import lombok.RequiredArgsConstructor;
-//
-//import java.util.ArrayList;
-//import java.util.Date;
-//import java.util.List;
-//import java.util.Optional;
-//
-//@RequiredArgsConstructor
-//public class AccountServiceImpl extends AbstractService<Account> implements AccountService {
-//
-//    private final List<Account> accountDTOList;
-//
-//    @Override
-//    public String withdraw(Account account) {
-//        return accountDTOList.stream()
-//                .filter(i -> i.getAccountNumber().equals(account.getAccountNumber()))
-//                .filter(i -> i.getBalance() >= account.getBalance())
-//                .peek(i -> {
-//                    i.setBalance(i.getBalance() - account.getBalance());
-//                    i.setTransactionDate(new Date());
-//                })
-//                .findAny().isPresent() ? "Withdraw success" : "Withdraw fail";
-//    }
-//
-//    @Override
-//    public String deposit(Account account) {
-//        return accountDTOList.stream()
-//                .filter(i -> i.getAccountNumber().equals(account.getAccountNumber()))
-//                .peek(i -> {
-//                    i.setBalance(i.getBalance() + account.getBalance());
-//                    i.setTransactionDate(new Date());
-//                })
-//                .findAny().isPresent() ? "Deposit success" : "Deoposit fail";
-//    }
-//
-//    @Override
-//    public String getBalance(Account account) {
-//        return accountDTOList.stream()
-//                .filter(i -> i.getAccountNumber().equals(account.getAccountNumber()))
-//                .map(i -> "Balance of " + i.getAccountHolder() + " : " + i.getBalance())
-//                .findFirst()
-//                .orElse("We can't find your com.erichgamma.api.account");
-//    }
-//
-//
-//    @Override
-//    public Messenger save(Account account) {
-//        accountDTOList.add(account);
-//        return Messenger.SUCCESS;
-//    }
-//
-//    @Override
-//    public Messenger delete(Account account) {
-//        return accountDTOList.removeIf(i -> i.getAccountNumber().equals(account.getAccountNumber())) ?
-//                Messenger.SUCCESS : Messenger.FAIL;
-//    }
-//
-//    @Override
-//    public Messenger deleteAll() {
-//        accountDTOList.clear();
-//        return Messenger.SUCCESS;
-//    }
-//
-//    @Override
-//    public List<Account> findAll() {
-//        return new ArrayList<>(accountDTOList);
-//    }
-//
-//    @Override
-//    public Optional<Account> findById(Long id) {
-//        return Optional.empty();
-//    }
-//
-//    @Override
-//    public Long count() {
-//        return (long) accountDTOList.size();
-//    }
-//
-//    @Override
-//    public Optional<Account> getOne(String id) {
-//        return accountDTOList.stream()
-//                .filter(i -> i.getAccountNumber().equals(id))
-//                .findAny();
-//    }
-//
-//    @Override
-//    public Boolean existsById(Long id) {
-//        return null;
-//    }
-//}
+package com.erichgamma.api.account;
+
+import com.erichgamma.api.common.AbstractService;
+import com.erichgamma.api.enums.Messenger;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AccountServiceImpl extends AbstractService<Account> implements AccountService {
+
+    private final List<Account> accountDTOList;
+
+    @Override
+    public String withdraw(Account account) {
+        return accountDTOList.stream()
+                .filter(i -> i.getAccountNumber().equals(account.getAccountNumber()))
+                .filter(i -> i.getBalance() >= account.getBalance())
+                .peek(i -> {
+                    i.setBalance(i.getBalance() - account.getBalance());
+                    i.setTransactionDate(new Date());
+                })
+                .findAny().isPresent() ? "Withdraw success" : "Withdraw fail";
+    }
+
+    @Override
+    public String deposit(Account account) {
+        return accountDTOList.stream()
+                .filter(i -> i.getAccountNumber().equals(account.getAccountNumber()))
+                .peek(i -> {
+                    i.setBalance(i.getBalance() + account.getBalance());
+                    i.setTransactionDate(new Date());
+                })
+                .findAny().isPresent() ? "Deposit success" : "Deoposit fail";
+    }
+
+    @Override
+    public String getBalance(Account account) {
+        return accountDTOList.stream()
+                .filter(i -> i.getAccountNumber().equals(account.getAccountNumber()))
+                .map(i -> "Balance of " + i.getAccountHolder() + " : " + i.getBalance())
+                .findFirst()
+                .orElse("We can't find your com.erichgamma.api.account");
+    }
+
+
+    @Override
+    public Messenger save(Account account) {
+        accountDTOList.add(account);
+        return Messenger.SUCCESS;
+    }
+
+    @Override
+    public Messenger delete(Account account) {
+        return accountDTOList.removeIf(i -> i.getAccountNumber().equals(account.getAccountNumber())) ?
+                Messenger.SUCCESS : Messenger.FAIL;
+    }
+
+    @Override
+    public Messenger deleteAll() {
+        accountDTOList.clear();
+        return Messenger.SUCCESS;
+    }
+
+    @Override
+    public List<Account> findAll() {
+        return new ArrayList<>(accountDTOList);
+    }
+
+    @Override
+    public Optional<Account> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Long count() {
+        return (long) accountDTOList.size();
+    }
+
+    @Override
+    public Optional<Account> getOne(String id) {
+        return accountDTOList.stream()
+                .filter(i -> i.getAccountNumber().equals(id))
+                .findAny();
+    }
+
+    @Override
+    public Boolean existsById(Long id) {
+        return null;
+    }
+}
